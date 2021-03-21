aaf0c1b354a38072af6fa3de5268cc86b0cb0e5e
nils-vanderwerf <n.vanderw.92@gmail.com>
2021-03-21 16:19:38 +1100

    Passing all tests

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Your code here
+
+class Cat {
+    constructor(name, sex) {
+        this.name = name
+        this.sex = sex
+    }
+
+    speak(){
+        return `${this.name} says meow!`
+    }
+}
+
+class Dog {
+    constructor(name, sex) {
+        this.name = name
+        this.sex = sex
+    }
+
+    speak() {
+        return `${this.name} says woof!`
+    }
+}
+
+class Bird {
+    constructor(name, sex) {
+        this.name = name
+        this.sex = sex
+    }
+
+    speak() {
+        if (this.sex === 'male') {
+        return `It's me! ${this.name}, the parrot!`
+        } else {
+        return `${this.name} says squawk!`
+        }
+    }
+}
+
+let George = new Cat("George", 'male')
+let Bob = new Dog("Bob", 'male')
+let Steve = new Bird("Steve", 'male')
+let Mable = new Bird("Mable", 'female')
+
+console.log(`George: ${George}, Bob: ${Bob}, Steve: ${Steve}, Mable: ${Mable}`)
